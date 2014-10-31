9046b89d4c3d7c15cd2784bf2f9a6c451cb13327
Caleb Meyer <caleb.meyer@cerner.com>
2014-10-30 21:33:38 -0500

    Added variable color. Fixed string interpolation.

--- example.rb
+++ example.rb
@@ -3,10 +3,16 @@ class Example
   CONSTANT = true
 
   @@class_variable = 1
-  @instance_variable = "String with #{interpolation}"
+  @instance_variable = "String with #{interpolation} and e\sc\ap\e characters"
 
   def method_definition(parameter, and_again)
 
   end
+
+  method_call
+  predicate?
+  unsafe_method_call!
+
+  method_call_with_parentheses()
 
 end
