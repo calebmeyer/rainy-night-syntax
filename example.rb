1b3a8c1f50ba25356b51ab4d324367ce4912271f
Caleb Meyer <caleb.meyer@cerner.com>
2014-10-30 21:12:00 -0500

    Added language ruby and example file. Added coloring for strings.

---
+++ example.rb
@@ -0,0 +1,12 @@
+class Example
+  # single line comment
+  CONSTANT = true
+
+  @@class_variable = 1
+  @instance_variable = "String with #{interpolation}"
+
+  def method_definition(parameter, and_again)
+
+  end
+
+end
